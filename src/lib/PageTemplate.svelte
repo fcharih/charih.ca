5f38da78e6a49a3a2cd27a2e7508dbf4a288b185
Francois Charih <francois@charih.ca>
2024-11-16 20:47:54 -0500

    Update music pages

--- src/lib/PageTemplate.svelte
+++ src/lib/PageTemplate.svelte
@@ -68,36 +68,6 @@
 
 <style>
   /*=============== GLOBAL =================*/
-  @font-face {
-    font-family: "Libertinus Sans";
-    font-style: bold;
-    src: url("/LibertinusSans-Bold.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "Libertinus Sans";
-    font-style: normal;
-    src: url("/LibertinusSans-Regular.otf") format("opentype");
-  }
-
-  @font-face {
-    font-family: "IBM Plex Serif";
-    font-style: normal;
-    src: url("https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap")
-      format("opentype");
-  }
-
-  @font-face {
-    font-family: "IBM Plex Sans";
-    font-style: normal;
-    src: url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700")
-      format("opentype");
-  }
-
-  :global(body) {
-    /* applies to <body> */
-    font-family: "IBM Plex Serif";
-  }
 
   :global(h1, h2, h3) {
     text-align: center;
--- src/routes/fr/musique/+page.svelte
+++ src/routes/fr/musique/+page.svelte
@@ -4,4 +4,30 @@
 
 <PageTemplate>
   <h1>Musique</h1>
+
+  <p>
+    Dans mes temps libres, j'aime écrire de la musique. Je compose des chansons
+    depuis l'adolescence.
+  </p>
+  <p>
+    Mes études ont accaparé beaucoup de mon temps, ce qui m'en a laissé trop peu
+    pour jouer et écrire des chansons. L'art et la musique sont importants, mais
+    combattre les maladies l'est aussi. Comme j'approche la fin de mon doctorat,
+    je suis en train de me remettre à l'écriture.
+  </p>
+  <p>
+    Vous pouvez écouter mon dernier single
+    <i>Half Moon Eyes</i> ici en attendant.
+  </p>
+
+  <div>
+    <iframe
+      style="border: 0; width: 300px; height: 470px; display: block; margin: 20px auto;"
+      src="https://bandcamp.com/EmbeddedPlayer/album=1272590194/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
+      seamless
+      ><a href="https://itsfrancois31.bandcamp.com/album/half-moon-eyes-single"
+        >Half Moon Eyes (Single) by Francois 31</a
+      ></iframe
+    >
+  </div>
 </PageTemplate>
