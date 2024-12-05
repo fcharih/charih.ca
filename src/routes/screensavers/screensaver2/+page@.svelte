a13c149c41ee46527656d578ee5acf6859febd78
Francois Charih <francois@charih.ca>
2024-12-05 11:34:48 -0500

    Screensaver2 done

--- src/routes/screensavers/screensaver2/+page@.svelte
+++ src/routes/screensavers/screensaver2/+page@.svelte
@@ -1,60 +1,57 @@
 <script>
+  import { onMount } from "svelte";
   import { parse } from "svgson";
-  import Logo from "$lib/NuvoBioLogo.svg?raw";
+  import Logo from "./Logo.svelte";
 
-  let refs = $state([]);
   let innerWidth = $state(0);
   let innerHeight = $state(0);
 
-  const LogoJson = parse(Logo);
-  LogoJson.then((x) => {
-    let STEP = 0.1;
-
-    let position = $state({ x: 200, y: 200 });
-    let opacity = $state(0);
-    let opacityIncreasing = $state(true);
-    let logoWidth = $state(300);
-    let logoHeight = $state(300);
-
-    function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+  let collide = (elem1, elem2) => {
+    let argminx = [elem1.x, elem2.x].indexOf(Math.min(elem1.x, elem2.x));
+    let argminy = [elem1.y, elem2.y].indexOf(Math.min(elem1.y, elem2.y));
+    let elem = [elem1, elem2][argminx];
+    let other = [elem1, elem2][!argminx];
+    if (elem.x + elem.width < other.x) {
+      return false;
     }
+    elem = [elem1, elem2][argminy];
+    other = [elem1, elem2][!argminy];
+    if (elem.y + height < other.y) {
+      return false;
+    }
+    return true;
+  };
 
-    const generateRandomPosition = () => {
-      const random1 = getRandomInt(0, innerWidth - logoWidth);
-      const random2 = getRandomInt(0, innerHeight - logoHeight);
+  let logoWidth = 0.25 * 1418;
+  let logoHeight = 0.25 * 482;
 
-      return {
-        x: random1,
-        y: random2,
-      };
-    };
-
-    setInterval(() => {
-      const newOpacity = opacityIncreasing ? opacity + STEP : opacity - STEP;
-      if (newOpacity > 1) {
-        opacityIncreasing = !opacityIncreasing;
-        return;
-      }
-
-      if (newOpacity < 0) {
-        opacityIncreasing = !opacityIncreasing;
-        position = generateRandomPosition();
-        return;
-      }
-      opacity = newOpacity;
-    }, 100);
-  });
+  let positions = $state([
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+  ]);
 </script>
 
-<svelte:window bind:innerWidth bind:innerHeight />
 <svelte:head></svelte:head>
+<svelte:window bind:innerWidth bind:innerHeight />
 
 <div id="container">
-  {#each refs as ref}
-    ref()
+  {#each positions as position, i}
+    <Logo
+      bind:positions
+      index={i}
+      step={0.1}
+      interval={10}
+      waitTime={3000}
+      scale={0.2}
+      dimensions={{ width: logoWidth, height: logoHeight }}
+      windowSize={{ width: innerWidth, height: innerHeight }}
+    />
   {/each}
 </div>
 
@@ -63,9 +60,5 @@
     height: 100%;
     width: 100%;
     background-color: white;
-  }
-
-  #logo {
-    position: absolute;
   }
 </style>
