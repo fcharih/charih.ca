f944397ab80d3b133d1ca5a0e126656bfdc146fd
Francois Charih <francois@charih.ca>
2025-09-15 22:20:53 -0400

    CV is served by Contentful

--- src/routes/(app)/cv/+page.svelte
+++ src/routes/(app)/cv/+page.svelte
@@ -2,14 +2,64 @@
   import { locale } from "../i18n";
   import yamlCV from "$lib/content/cv.yaml?raw";
   import yaml from "js-yaml";
+  import * as contentful from "contentful";
+  import { marked } from "marked";
+  import {
+    PUBLIC_CONTENTFUL_SPACE_ID,
+    PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  } from "$env/static/public";
 
   let cv = yaml.load(yamlCV);
   let journalPubs = cv.publications.filter((x) => x.type === "journal");
   let conferencePubs = cv.publications.filter((x) => x.type === "conference");
-  let preprints = cv.publications.filter((x) => x.type === "preprint");
-  let presentations = cv.presentations;
-  let posters = cv.posters;
-  let webtools = cv.webtools;
+  //let preprints = cv.publications.filter((x) => x.type === "preprint");
+  //let presentations = cv.presentations;
+  //let posters = cv.posters;
+  //let webtools = cv.webtools;
+
+  const client = contentful.createClient({
+    space: PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  let journalPublications = $state([]);
+  let conferencePublications = $state([]);
+  let presentations = $state([]);
+  let webtools = $state([]);
+  let preprints = $state([]);
+  let posters = $state([]);
+
+  client
+    .getEntries()
+    .then((response) => {
+      journalPublications = response.items.filter(
+        (x) =>
+          x.sys.contentType.sys.id == "publication" &&
+          x.fields.publicationType == "Journal"
+      );
+      conferencePublications = response.items.filter(
+        (x) =>
+          x.sys.contentType.sys.id == "publication" &&
+          x.fields.publicationType == "Conference"
+      );
+      preprints = response.items.filter(
+        (x) =>
+          x.sys.contentType.sys.id == "publication" &&
+          x.fields.publicationType == "Preprint"
+      );
+      presentations = response.items.filter(
+        (x) => x.sys.contentType.sys.id == "presentation"
+      );
+      posters = response.items.filter(
+        (x) => x.sys.contentType.sys.id == "poster"
+      );
+      webtools = response.items.filter(
+        (x) => x.sys.contentType.sys.id == "softwareAndWebTools"
+      );
+
+      console.log(presentations);
+    })
+    .catch(console.error);
 </script>
 
 <svelte:head>
@@ -32,28 +82,33 @@
 <h3>
   {$locale === "en" ? "Peer-reviewed journals" : "Articles de revue savante"} ({journalPubs.length})
 </h3>
-{#each journalPubs as pub, i}
+{#each journalPublications.toSorted((a, b) => b.fields.year - a.fields.year) as pub, i}
   <div class="journal-pub">
-    <b>[J{journalPubs.length - i}]</b>
-    {pub.authors}. {#if pub.url}<a href={pub.url}>{pub.title}</a
-      >{:else}{pub.title}{/if} ({pub.year}).
-    <i>{pub.publisher}</i>{#if pub.volume !== ""}
-      {" "}{pub.volume}{/if}{#if pub.issue !== ""}({pub.issue}){/if}{#if pub.pages !== ""},
-      pp. {pub.pages}{/if}.
+    <b>[J{journalPublications.length - i}]</b>
+    {pub.fields.authors.join(", ")}. {#if pub.fields.url}<a
+        href={pub.fields.url}>{pub.fields.title}</a
+      >{:else}{pub.fields.title}{/if} ({pub.fields.year}).
+    <i>{pub.fields.publisher}</i>{#if pub.fields.volume}
+      {" "}{pub.fields.volume}{/if}{#if pub.fields.issue}({pub.fields
+        .issue}){/if}{#if pub.fields.pages}, pp. {pub.fields
+        .pages}{/if}{#if pub.fields.status !== "Published"}{" "}({pub.fields
+        .status}){/if}.
   </div>
 {/each}
 
 <h3>
   {$locale === "en"
     ? "Conference proceedings"
-    : "Comptes-rendus de conférences"} ({conferencePubs.length})
+    : "Comptes-rendus de conférences"} ({conferencePublications.length})
 </h3>
-{#each conferencePubs as pub, i}
+{#each conferencePublications.toSorted((a, b) => b.fields.year - a.fields.year) as pub, i}
   <div class="conference-pub">
     <b>[C{conferencePubs.length - i}]</b>
-    {pub.authors}. {#if pub.url}<a href={pub.url}>{pub.title}</a
-      >{:else}{pub.title}{/if}.
-    <i>{pub.conference}</i>, {pub.location}, {pub.date}.
+    {pub.fields.authors.join(", ")}. {#if pub.fields.url}<a
+        href={pub.fields.url}>{pub.fields.title}</a
+      >{:else}{pub.fields.title}{/if}.
+    <i>{pub.fields.conference}</i>, {pub.fields.location}, {pub.fields
+      .conferenceDate}.
   </div>
 {/each}
 
@@ -62,12 +117,14 @@
 </h3>
 
 {#if preprints.length === 0}No pre-prints at this time...{/if}
-{#each preprints as pub, i}
+{#each preprints.toSorted((a, b) => parseInt(b.fields.year) - parseInt(a.fields.year)) as pub, i}
   <div class="preprint">
     <b>[PP{preprints.length - i}]</b>
-    {pub.authors}. {#if pub.url}<a href={pub.url}>{pub.title}</a
-      >{:else}{pub.title}{/if} ({pub.date}).
-    <i>{pub.server}</i>.{#if pub.comment}{" "}({pub.comment}).{/if}
+    {pub.fields.authors.join(", ")}. {#if pub.fields.url}<a
+        href={pub.fields.url}>{pub.fields.title}</a
+      >{:else}{pub.fields.title}{/if},
+    <i>{pub.fields.publisher}</i>, {pub.fields
+      .year}.{#if pub.fields.comment}{" "}({pub.fields.comment}).{/if}
   </div>
 {/each}
 
@@ -76,24 +133,25 @@
     ? "Workshops and presentations"
     : "Ateliers et présentations"} ({presentations.length})
 </h2>
-{#each presentations as presentation, i}
+{#each presentations.toSorted((a, b) => new Date(b.fields.date) - new Date(a.fields.date)) as presentation, i}
   <div class="presentation">
-    <div><b>[W{presentations.length - i}]</b> {presentation.title}</div>
-    <div>{presentation.event}</div>
-    <div>{presentation.location}</div>
-    <div>{presentation.date}</div>
-    {#if presentation.url}<a href={presentation.url}>[Link]</a>{/if}
+    <div><b>[W{presentations.length - i}]</b> {presentation.fields.title}</div>
+    <div>{presentation.fields.event}</div>
+    <div>{presentation.fields.location}</div>
+    <div>{presentation.fields.date}</div>
+    {#if presentation.fields.url}<a href={presentation.fields.url}>[Link]</a
+      >{/if}
   </div>
 {/each}
 
 <h2>Posters ({posters.length})</h2>
-{#each posters as poster, i}
+{#each posters.toSorted((a, b) => new Date(b.fields.date) - new Date(a.fields.date)) as poster, i}
   <div class="poster">
-    <div><b>[P{posters.length - i}]</b> {poster.title}</div>
-    <div>{poster.event}</div>
-    <div>{poster.location}</div>
-    <div>{poster.date}</div>
-    {#if poster.url}<a href={poster.url}>[Link]</a>{/if}
+    <div><b>[P{posters.length - i}]</b> {poster.fields.title}</div>
+    <div>{poster.fields.event}</div>
+    <div>{poster.fields.location}</div>
+    <div>{poster.fields.date}</div>
+    {#if poster.fields.url}<a href={poster.fields.url}>[Link]</a>{/if}
   </div>
 {/each}
 
@@ -103,9 +161,10 @@
 {#each webtools as wt, i}
   <div class="webtool">
     <div>
-      <b>[WT{webtools.length - i}]</b> <a href={wt.url}>{wt.title}</a>
+      <b>[WT{webtools.length - i}]</b>
+      <a href={wt.fields.url}>{wt.fields.name}</a>
     </div>
-    <div>{wt.description}</div>
+    <div>{wt.fields.description}</div>
   </div>
 {/each}
 
